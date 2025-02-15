f9e3b70db5dff1f92cff56d18ff8d9d700531fbc
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2025-02-15 20:05:48 +0530

    separated the model into three components

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  encapsulation: ViewEncapsulation.None
+})
+export class HeaderComponent {
+
+}
---
+++ src/app/components/todo-tickets/todo-tickets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-tickets',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-tickets.component.html',
+  styleUrl: './todo-tickets.component.css'
+})
+export class TodoTicketsComponent {
+
+}
